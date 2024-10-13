636b17133528e5416dc11ec6197b15bbcfe4671d
Giovanni Saavedra Valdes <giovannisaavedra160@gmail.com>
2024-10-13 17:18:28 -0500

    Commit

--- WPFApp/MainWindow.xaml.cs
+++ WPFApp/MainWindow.xaml.cs
@@ -19,6 +19,56 @@ namespace WPFApp
         public MainWindow()
         {
             InitializeComponent();
+            CreateTask();
+        }
+
+        void AddMessage(string message)
+        {
+            int CurrentThreadId = Thread.CurrentThread.ManagedThreadId;
+            this.Dispatcher.Invoke(() =>
+            {
+                Messages.Content +=
+                    $"Mensaje: {message}, " +
+                    $"Hilo actual: {CurrentThreadId}\n";
+            });
+        }
+
+        void CreateTask()
+        {
+            Task T;
+
+            Action Code = new Action(ShowMesage);
+            T = new Task(Code);
+            Task T2 = new Task(delegate
+            {
+                MessageBox.Show("Ejecutando el metodo ShowMessage");
+            }
+            );
+            Task T3A = new Task(ShowMesage);
+            Task T3 = new Task(
+              () => ShowMesage());
+
+            Task T4 = new Task(() => MessageBox.Show("Ejecutando la tarea 4"));
+
+            Task T5 = new Task(() =>
+            {
+                DateTime CurrentDate = DateTime.Today;
+                DateTime StartDate = CurrentDate.AddDays(30);
+                MessageBox.Show($"Tarea 5. Fecha calculada: {StartDate}");
+            }    
+            );
+
+            Task T6 = new Task((message) =>
+            MessageBox.Show(message.ToString()), "Expresion lambda con parametros");
+
+            Task T7 = new Task(() => AddMessage("Ejecutando la tarera."));
+            T7.Start();
+            AddMessage("En el hilo principal");
+        }
+
+        void ShowMesage()
+        {
+            MessageBox.Show("Ejecutando el metodo ShowMessage");
         }
     }
 }
